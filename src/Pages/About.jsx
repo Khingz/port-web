2c285962bd59ed921d26011981903388c286ee17
Khingz <kingsley995mr@gmail.com>
2022-07-17 17:34:59 +0100

    timeline for contact added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,11 @@
+import './About.css'
+
 const about = () => {
   return (
-    <div>
-       About
+    <div className="about-container">
+       <div className="about">
+         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse eius ex fugit accusamus quidem, saepe eaque atque ipsum, vel obcaecati cumque quae eos, pariatur quia animi sequi asperiores? Minima, quisquam.
+       </div>
     </div>
   )
 }

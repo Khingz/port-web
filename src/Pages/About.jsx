6a4ff89adee48c2ea3caac3f6ec650504ded01e9
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2022-09-14 11:18:17 +0100

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -14,7 +14,7 @@ const about = () => {
         <div className="about-content">
         <p>
           Code lover and a fan of the internet!! <span style={{color:'#db9200'}}>Hey there!</span> My name is Kingsley from Nigeria. I'm a freelancing web developer that has worked on sites for small and medium-sized enterprises. I can assist you with finding jobs, listing your services, or even setting up a whole internet business.
-          HTML and CSS 3, Vanilla javascript, React, Node and Express are all skills I have. As a developer, i love contributong to open source projects, especially the ones that are challenging and help me learn great deal of new technologies.
+          HTML and CSS 3, Vanilla javascript, React, Node and Express are all skills I have. As a developer, i love contributing to open source projects, especially the ones that are challenging and help me learn great deal of new technologies.
           I'll oversee the entire job from conception to end.
           I value regular communication very much, so let's stay in touch!
          </p>
